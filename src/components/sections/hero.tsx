95998148c89f216ca13d748bd90db9da20ce2f25
Anmol Patel <anmolpatel234@gmail.com>
2025-07-18 20:05:26 +0000

    show both in phone view too

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -34,7 +34,7 @@ export default function Hero() {
             </p>
           </div>
 
-          <div className="relative w-full h-96 hidden md:flex items-center justify-center gap-8">
+          <div className="relative w-full h-[550px] md:h-96 flex flex-col md:flex-row items-center justify-center gap-8">
               {/* Rotating Earth */}
               <div className="relative h-80 w-80">
                   <div className="absolute inset-0 rounded-full overflow-hidden shadow-lg">
