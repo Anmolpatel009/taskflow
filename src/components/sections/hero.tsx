b900f1b8b57f3301c64baa68d2fb008e1f6f0e9e
Anmol Patel <anmolpatel234@gmail.com>
2025-07-18 11:31:17 +0000

    name find talent as show all freelancer than fetch all form user collect

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -18,10 +18,10 @@ export default function Hero() {
             </p>
             <div className="flex flex-col sm:flex-row gap-4 justify-center md:justify-start">
               <Button asChild size="lg" className="bg-white text-primary hover:bg-white/90">
-                <Link href="/showall">Find Talent</Link>
+                <Link href="/showall">Show All Freelancers</Link>
               </Button>
               <Button asChild size="lg" variant="secondary">
-                <Link href="/nearby">Find Work</Link>
+                <Link href="/nearby">Find Nearby</Link>
               </Button>
             </div>
           </div>
