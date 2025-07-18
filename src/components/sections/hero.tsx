b89135d070defb90fa7da5fd6e9ed01fcc59d1f6
Anmol Patel <anmolpatel234@gmail.com>
2025-07-18 17:03:16 +0000

    wher is image ?

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -89,7 +89,7 @@ export default function Hero() {
               {/* Front square image */}
               <div className="absolute top-0 left-10 h-64 w-64 rounded-lg overflow-hidden shadow-2xl z-20">
                   <Image
-                      src="https://placehold.co/600x400.png"
+                      src="https://images.pexels.com/photos/3184418/pexels-photo-3184418.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
                       data-ai-hint="diverse professionals"
                       alt="A pool of diverse professionals"
                       fill
